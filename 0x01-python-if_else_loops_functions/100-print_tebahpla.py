a8ba4ca7aab609f44dd79ad4ad99ffd4f364f0d2
johnrand <johnrandy0652@gmail.com>
2024-01-04 05:52:25 +0300

    Fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,5 @@
 
 # print alphabet in reverse and alternate lower with upper
 
-for i in range(ord('Z'), ord('A') - 1, -1):
+for i in range(ord('z'), ord('A') - 1, -1)[:26]:
     print("{}".format("".join(chr(i) if i % 2 == 0 else chr(i - 32))), end="")
