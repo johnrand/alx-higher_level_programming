b6caedde5c218202e114aff8a3f92af218d237d7
johnrand <johnrandy0652@gmail.com>
2024-01-08 16:39:49 +0300

    function to print items in a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    print_list_integer:
+                        function to print integers in a list
+    my_list:
+            list to be printed
+"""
+
+
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{}".format(i))
