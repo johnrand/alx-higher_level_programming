945dae6bbbcb8d8a9fca9326753ad5abf4976c0b
johnrand <johnrandy0652@gmail.com>
2024-01-10 23:33:44 +0300

    update a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+    update_dictionary:
+        function that replaces or adds key/value in a dictionary
+    a_dictionary:
+        the dictionay
+    key:
+        argument will be always a string
+    value:
+        argument will be any type
+"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
