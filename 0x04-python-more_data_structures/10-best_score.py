25b264551ef40d6cdeff47431db9618e2065f40d
johnrand <johnrandy0652@gmail.com>
2024-01-10 23:57:37 +0300

    biggest value in a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    best_score:
+        function that returns a key with the biggest integer value
+        multiplied by 2
+    a_dictionary:
+        the dictionay
+"""
+
+
+def best_score(a_dictionary):
+    if not in a_dictionary:
+        return None
+
+    biggest_int = max(a_dictionary, key=a_dictionary.get)
+    return biggest_int
