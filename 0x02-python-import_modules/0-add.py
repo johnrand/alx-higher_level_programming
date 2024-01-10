a2b96156ac4ffa2f89bd05a3a53f68d57385387e
johnrand <johnrandy0652@gmail.com>
2024-01-11 00:51:37 +0300

    Initial commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 """Program to import modules
-    
+
     Add():
         calling a function
 """
-
 a = 1
 b = 2
-
-add_0 = __import('add_o').add
-print(f"{a} + {b} = {add(a, b)}")
+add_0 = __import__('add_o')
+result = add_0.add(a, b)
+print("{} + {} = {}".format(a, b, result))
