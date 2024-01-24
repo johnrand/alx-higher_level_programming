f9c8651ee1cac98430fcedcb454c803e650f574d
johnrand <johnrandy0652@gmail.com>
2024-01-25 01:05:30 +0300

    Initial commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,44 @@
+class Square:
+    """
+    This is the Square class.
+
+    It represents a square and is currently empty, serving as a placeholder.
+    """
+
+    def __init__(self):
+        """
+        The constructor for the Square class.
+
+        Currently, it initializes an empty square object.
+        """
+        pass
+
+    def square_fnctn(self):
+        """
+        Example method inside the Square class.
+
+        This method does nothing.
+        """
+        pass
+
+
+def square_fnctn():
+    """
+    Example function outside the Square class.
+
+    This function does nothing.
+    """
+    pass
+
+
+# Print documentation for the module
+print(__import__("__main__").__doc__)
+
+# Print documentation for the Square class
+print(__import__("__main__").Square.__doc__)
+
+# Print documentation outside the class
+print(__import__("__main__").square_fnctn.__doc__)
+
+# Print documentation inside the Square class
+print(__import__("__main__").Square.square_fnctn.__doc__)
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+class Square:
+    """
+    This is the Square class.
+
+    It defines a square with a private instance attribute 'size'.
+    """
+
+    def __init__(self, size):
+        """
+        The constructor for the Square class.
+
+        Args:
+            size (int): The size of the square.
+
+        Note:
+            The size is a private attribute.
+        """
+        self.__size = size
