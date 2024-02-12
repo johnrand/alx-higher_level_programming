7cc0eb864d46d219c5b5d9d6e77f34fb5015ea00
johnrand <johnrandy0652@gmail.com>
2024-02-13 02:31:34 +0300

    Initial commit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,52 +1,15 @@
 #!/usr/bin/python3
 
+"""
+This is a module of an empty square class
+
+"""
+
+
 class Square:
-    """This is the Square class.
-
+    """
     It represents a square and is currently empty, serving as a placeholder.
+
     """
 
-    def __init__(self):
-        """The constructor for the Square class.
-
-        Currently, it initializes an empty square object.
-        """
-        pass
-
-    def square_fnctn(self):
-        """Example method inside the Square class.
-
-        This method does nothing.
-        """
-        pass
-
-    @property
-    def dict_(self):
-        """Getter method for obtaining a dictionary representation
-        of the square
-
-        Returns:
-            dict: A dict containing info about the suare
-        """
-        return {}
-
-
-def square_fnctn():
-    """Example function outside the Square class.
-
-    This function does nothing.
-    """
     pass
-
-
-# Print documentation for the module
-print(__doc__)
-
-# Print documentation for the Square class
-print(Square.__doc__)
-
-# Print documentation outside the class
-print(square_fnctn.__doc__)
-
-# Print documentation inside the Square class
-print(Square.square_fnctn.__doc__)
