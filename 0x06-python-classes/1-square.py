44972f281bc241885d8acea4bcb2b25117b841ce
johnrand <johnrandy0652@gmail.com>
2024-02-13 02:36:44 +0300

    Initial commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
 
-class Square:
-    """This is the Square class.
+"""
+This module defines a square class with a private instance
 
+"""
+
+
+class Square:
+    """
     It defines a square with a private instance attribute 'size'.
+
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size=None):
         """The constructor for the Square class.
 
         Args:
-            size (int): The size of the square.
+            size: The size of the square.
 
         Note:
             The size is a private attribute.
         """
         self.__size = size
-
-    @property
-    def dict_(self):
-        """Getter method for obtaining a dictionaty representation of
-        the square class.
-
-        Returns:
-            dict: A dictionary containing the size of the square.
-        """
-        return ('size': self.__size)
-
-
-# Print documentation for the module
-print(__doc__)
-
-# Print documentation for the Square class
-print(Square.__doc__)
