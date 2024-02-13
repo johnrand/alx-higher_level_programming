79a764065e55c1a51ef3545758503a4c319f15eb
johnrand <johnrandy0652@gmail.com>
2024-02-13 03:20:13 +0300

    Initial commit

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""
+This module validate the size attribute and raises error if
+not an integeri and returns the square
+
+"""
+
+
+class Square:
+    """
+    This class validates the size attribute and raises an error
+
+    Attributes:
+        size: private instance must be integer
+    """
+    def __init__(self, size=0):
+
+        @property
+        def size(self):
+            """
+            Getter to return the size
+
+            """
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            """
+            Setter to return the value
+            Vallu: value returned
+
+            """
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
+    def area(self):
+        """
+        Takes no arguments
+        Return the area of the square
+
+        """
+        return self.__size ** 2
