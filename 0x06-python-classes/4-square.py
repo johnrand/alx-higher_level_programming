b57d7feda1bccf7d97e028a81dae0e478f2db0b0
johnrand <johnrandy0652@gmail.com>
2024-02-13 03:28:41 +0300

    Fix value error

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -34,7 +34,7 @@ class Square:
             """
             if not isinstance(value, int):
                 raise TypeError("size must be an integer")
-            if size < 0:
+            if value < 0:
                 raise ValueError("size must be >= 0")
             self.__size = value
 
