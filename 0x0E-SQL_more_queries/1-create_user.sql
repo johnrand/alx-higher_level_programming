0029906e2970409a230f5dc57bc9bbc775fdc0f2
johnrand <johnrandy0652@gmail.com>
2024-03-14 01:14:30 +0300

    Initial commit

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,11 +4,11 @@
 -- If the user user_0d_1 already exists, your script should not fail
 
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost`
-IDENTIFIED BY `user_0d_1_pwd`;
+IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON `*.*`
+GRANT ALL PRIVILEGES ON *.*
 TO `user_0d_1`@`localhost` 
-IDENTIFIED BY `user_0d_1_pwd`
+IDENTIFIED BY 'user_0d_1_pwd'
 WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
