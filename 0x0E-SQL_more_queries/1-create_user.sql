355344c14d6f1d7448235225faa15993617f3f1b
johnrand <johnrandy0652@gmail.com>
2024-03-14 01:18:03 +0300

    Initial commit

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -6,9 +6,8 @@
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost`
 IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.*
-TO `user_0d_1`@`localhost` 
-IDENTIFIED BY 'user_0d_1_pwd'
-WITH GRANT OPTION;
+GRANT PROXY
+ON root
+TO `user_0d_1`@`localhost`;
 
 FLUSH PRIVILEGES;
