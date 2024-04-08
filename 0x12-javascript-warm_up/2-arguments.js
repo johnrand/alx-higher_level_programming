dbd5b699b22da0adc06f0b1c5278e6219caaa1de
johnrand <johnrandy0652@gmail.com>
2024-04-08 22:16:18 +0300

    Initial commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/*
+ * Subtract 2 to account for node and file
+ */
+
+const argv = process.argv.length - 2;
+
+if ( argv === 0 ) {
+	console.log('No argument');
+} else if ( argv === 1 ) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
